1ca221a1b8afad2b94cc9492f5e40c109c9e260d
ChrisVL-W <chris.vanlanenwanek@gmail.com>
2023-11-16 18:32:40 -0600

    More interface things, pick the data we're interested in from Strava

--- app/routes/api.strava.ts
+++ app/routes/api.strava.ts
@@ -2,7 +2,8 @@ import { LoaderFunction, json } from "@remix-run/node";
 
 /**
  * Strava interface for activity data
- * For more info see https://developers.strava.com/playground/#/Activities/getLoggedInAthleteActivities
+ *
+ * For more info see: https://developers.strava.com/playground/#/Activities/getLoggedInAthleteActivities
  */
 export interface StravaActivity {
   /** The unique identifier of the activity */
@@ -98,8 +99,51 @@ export interface StravaActivity {
   weighted_average_watts: number;
 }
 
+// Interface representing the token data we recieve from Strava
+export interface TokenData {
+  /** A Strava access token used for requesting data */
+  access_token: string;
+  /** A Strava refresh token, used to request a new access/refresh token */
+  refresh_token: string;
+  /**
+   * A timestamp of when the access token expires, if expired use refresh token to request new tokens
+   *
+   * IMPORTANT: Strava returns this in a different format (seconds rather than milliseconds) so we convert it
+   * before saving it back to the database
+   */
+  expires_at: number;
+}
+
+export interface ProcessedActivityData
+  extends TokenData,
+    Pick<
+      StravaActivity,
+      | "name"
+      | "distance"
+      | "moving_time"
+      | "type"
+      | "average_speed"
+      | "start_date"
+    > {
+  /** A timestamp representing when we last fetched activity data from Strava */
+  updated: number;
+}
+
+/**
+ * Loads data from Strava, cached every five minutes to avoid API rate limits (100/hr, 1000/day)
+ *
+ * Step by step breakdown:
+ * 1. Load existing data from the database
+ * 2. If that data is recent (within last 5 minutes) just return the data
+ * 3. Check the expiration on access token. If expired, request a new one using refresh token
+ * 4. Retrive new data from Strava endpoint
+ * 5. Process that data
+ * 6. Save the result (plus tokens and updated timestamp) back to the database
+ * 7. Return the newly processed data
+ *
+ * @returns JSON, either the processed activity data (if successful) or an error if one was encountered
+ */
 export const loader: LoaderFunction = async () => {
-  // Steps for loading data from Strava:
   const pantryId = process.env.PANTRY_ID;
   const newStravaBasket = process.env.NEW_STRAVA_BASKET;
   if (!pantryId || !newStravaBasket) {
@@ -108,39 +152,26 @@ export const loader: LoaderFunction = async () => {
       { status: 500 }
     );
   }
-  //  1. Load existing data from the database
-  const pantryData = await getPantry(pantryId, newStravaBasket);
-  const { access_token, refresh_token, expires_at, updated } = pantryData;
-  //  2. If that data is recent (within last 2 minutes) just return the data
-  //   if (updated > Date.now() - 1000 * 60 * 2) {
-  //     return json(pantryData);
+  const data = await getPantry(pantryId, newStravaBasket);
+  //   if (updated > Date.now() - 1000 * 60 * 5) {
+  //     return json(data);
   //   }
-  const newPantryData = { access_token, refresh_token, expires_at, updated };
-  //  3. Check the expiration on access token. If expired, request a new one using refresh token
-  if (expires_at < Date.now()) {
-    // await refreshStravaTokens
-    // update data in pantry
+  if (data.expires_at < Date.now()) {
     if (!process.env.STRAVA_CLIENT_ID || !process.env.STRAVA_CLIENT_SECRET) {
       return json({ error: "Environment variables missing" }, { status: 500 });
     }
-    const tokenResponse = await refreshStravaTokens(
+    const tokens = await refreshStravaTokens(
       process.env.STRAVA_CLIENT_ID,
       process.env.STRAVA_CLIENT_SECRET,
-      refresh_token
+      data.refresh_token
     );
-    newPantryData.expires_at = tokenResponse.expires_at * 1000;
-    newPantryData.refresh_token = tokenResponse.refresh_token;
-    newPantryData.access_token = tokenResponse.access_token;
+    data.expires_at = tokens.expires_at * 1000;
+    data.refresh_token = tokens.refresh_token;
+    data.access_token = tokens.access_token;
   }
-  newPantryData.updated = Date.now();
-
-  //  4. Retrive new data from Strava endpoint
-  const activities = await getStravaActivities(access_token);
-  //  5. Process that data
-  //  6. Save the result back to the database
-
-  const response = await putPantry(pantryId, newStravaBasket, newPantryData);
-  //  7. Return the newly processed data
+  const activities = await getStravaActivities(data.access_token);
+  data.updated = Date.now();
+  const response = await putPantry(pantryId, newStravaBasket, data);
   return response;
 };
 
@@ -148,7 +179,7 @@ const refreshStravaTokens = async (
   stravaClientId: string,
   stravaClientSecret: string,
   refreshToken: string
-) => {
+): Promise<TokenData> => {
   const response = await fetch(
     `https://www.strava.com/oauth/token?client_id=${stravaClientId}&client_secret=${stravaClientSecret}&grant_type=refresh_token&refresh_token=${refreshToken}`,
     { method: "POST" }
@@ -156,21 +187,30 @@ const refreshStravaTokens = async (
   return response.json();
 };
 
-const getStravaActivities = async (stravaAccessToken: string) => {
+const getStravaActivities = async (
+  stravaAccessToken: string
+): Promise<StravaActivity[]> => {
   const response = await fetch(
     `https://www.strava.com/api/v3/athlete/activities?access_token=${stravaAccessToken}`
   );
   return response.json();
 };
 
-const getPantry = async (pantryId: string, basketName: string) => {
+const getPantry = async (
+  pantryId: string,
+  basketName: string
+): Promise<ProcessedActivityData> => {
   const response = await fetch(
     `https://getpantry.cloud/apiv1/pantry/${pantryId}/basket/${basketName}`
   );
   return response.json();
 };
 
-const putPantry = async (pantryId: string, basketName: string, data: any) => {
+const putPantry = async (
+  pantryId: string,
+  basketName: string,
+  data: ProcessedActivityData
+) => {
   const response = await fetch(
     `https://getpantry.cloud/apiv1/pantry/${pantryId}/basket/${basketName}`,
     {
