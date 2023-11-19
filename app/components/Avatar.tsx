9fb8cde38cade8cb196f05ab6d2c103a4f291e78
ChrisVL-W <chris.vanlanenwanek@gmail.com>
2023-11-19 17:52:59 -0600

    Fix avatar picture

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -1,18 +1,15 @@
-export interface AvatarProps {
-  size?: number;
-  src: string;
+import { HasClassName } from "./commonInterfaces"
+
+export interface AvatarProps extends HasClassName {
+  size?: number
+  src: string
 }
 
-export default function Avatar({ size, src }: AvatarProps) {
+export default function Avatar({ size, src, className }: AvatarProps) {
   // Set default values
-  size ??= 100;
+  size ??= 100
 
   return (
-    <img
-      className={`avatarImage ${size > 100 ? "large" : ""}`}
-      height={size}
-      width={size}
-      src={src}
-    />
-  );
+    <img className={`avatarImage ${size > 100 ? "large" : ""} ${className}`} height={size} width={size} src={src} />
+  )
 }
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,62 +1,44 @@
-import { type MetaFunction } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
-import Avatar from "~/components/Avatar";
-import IconButton from "~/components/IconButton";
-import { ProcessedActivityData } from "./api.strava";
-import StravaActivity from "~/components/StravaActivity";
-import ThemeSwitcher from "~/components/ThemeSwitcher";
+import { type MetaFunction } from "@remix-run/node"
+import { useLoaderData } from "@remix-run/react"
+import Avatar from "~/components/Avatar"
+import IconButton from "~/components/IconButton"
+import { ProcessedActivityData } from "./api.strava"
+import StravaActivity from "~/components/StravaActivity"
+import ThemeSwitcher from "~/components/ThemeSwitcher"
 
 export const meta: MetaFunction = () => {
-  return [
-    { title: "Dashboard | Chris Van Lanen-Wanek" },
-    { name: "description", content: "Welcome to my dashboard" },
-  ];
-};
+  return [{ title: "Dashboard | Chris Van Lanen-Wanek" }, { name: "description", content: "Welcome to my dashboard" }]
+}
 
 export const loader = async ({ request }: { request: Request }) => {
-  const res = await fetch(`${request.url}api/strava`);
-  const stravaData: ProcessedActivityData = await res.json();
-  return stravaData;
-};
+  const res = await fetch(`${request.url}api/strava`)
+  const stravaData: ProcessedActivityData = await res.json()
+  return stravaData
+}
 
-const avatarImage =
-  "https://scontent-msp1-1.xx.fbcdn.net/v/t39.30808-6/384752132_6783851961653044_4416593039952130129_n.jpg?stp=cp6_dst-jpg&_nc_cat=103&ccb=1-7&_nc_sid=5f2048&_nc_ohc=9hPnm-v3vj4AX9Yfr6Q&_nc_ht=scontent-msp1-1.xx&oh=00_AfDnNaqr0qEcluXKtjS8Used6v7xfSnZ2YPeWioJrYEASg&oe=6554D885";
+const avatarImage = "https://i.imgur.com/4Ouflwg.jpg"
 
 export default function Index() {
-  const data = useLoaderData<typeof loader>();
+  const data = useLoaderData<typeof loader>()
 
   return (
     <>
       <div className="flex align-center justify-center mainHeader">
         <div style={{ position: "relative" }}>
-          <Avatar size={150} src={avatarImage} />
+          <Avatar className="chrisAvatar" size={150} src={avatarImage} />
           <img src="https://i.imgur.com/GHLcHKH.png" className="musicIcon" />
         </div>
         <div className="detailBox onImage">
           <div className="header--container">
             <h1 className="header--name">Chris Van Lanen-Wanek</h1>
-            <h2 className="header--jobTitle">
-              Software Engineer | Web Developer
-            </h2>
+            <h2 className="header--jobTitle">Software Engineer | Web Developer</h2>
           </div>
           <div className="flex iconBox">
-            <IconButton
-              href="https://www.linkedin.com/in/crvlwanek/"
-              iconKey="linkedIn"
-            />
+            <IconButton href="https://www.linkedin.com/in/crvlwanek/" iconKey="linkedIn" />
             <IconButton href="https://github.com/crvlwanek" iconKey="github" />
-            <IconButton
-              href="https://www.facebook.com/crvlwanek/"
-              iconKey="facebook"
-            />
-            <IconButton
-              href="https://www.instagram.com/crvlwanek/"
-              iconKey="instagram"
-            />
-            <IconButton
-              href="https://www.youtube.com/c/ChrisVLWanek"
-              iconKey="youtube"
-            />
+            <IconButton href="https://www.facebook.com/crvlwanek/" iconKey="facebook" />
+            <IconButton href="https://www.instagram.com/crvlwanek/" iconKey="instagram" />
+            <IconButton href="https://www.youtube.com/c/ChrisVLWanek" iconKey="youtube" />
           </div>
         </div>
       </div>
@@ -74,5 +56,5 @@ export default function Index() {
         </div>
       </div>
     </>
-  );
+  )
 }
