9b8ae07902d914ffc1c56cbcf5ea282b2ce1d5dd
ChrisVL-W <chris.vanlanenwanek@gmail.com>
2023-12-15 23:43:02 -0600

    Start about me page

--- app/routes/about-me.tsx
+++ app/routes/about-me.tsx
@@ -1,11 +1,25 @@
+const image = "https://i.imgur.com/7VP7DiQ.jpg"
+
 export default function AboutMe() {
   return (
     <>
-      <div className="w-full bg-slate-800 h-20 flex items-center justify-center text-white">
+      <div className="pt-24 w-full flex items-center justify-center flex-col">
         <h1>About Me</h1>
+        <div className="bg-surface w-full max-w-3xl min-h-4 rounded-xl overflow-hidden shadow-xl m-4 flex">
+          <img src={image} className=" h-[400px] aspect-{6 / 8} object-none " />
+          <div className="p-4">
+            <div>
+              This will be the About Me page! For now, I really don't have anything to say here so
+              it will just end up being lorem ipsum. Lorem ipsum dolor sit amet, consectetur
+              adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+              commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+              proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="w-full h-screen bg-red-600" />
-      <div className="w-full h-screen bg-blue-600" />
     </>
   )
 }
