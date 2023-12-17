0d4c13975d336f175ce04c54e6ce0db3eb193e5d
ChrisVL-W <chris.vanlanenwanek@gmail.com>
2023-12-16 22:26:57 -0600

    Flex fix-ish

--- app/routes/about-me.tsx
+++ app/routes/about-me.tsx
@@ -3,10 +3,10 @@ const image = "https://i.imgur.com/7VP7DiQ.jpg"
 export default function AboutMe() {
   return (
     <>
-      <div className="pt-24 w-full flex items-center justify-center flex-col">
-        <h1>About Me</h1>
-        <div className="bg-surface w-full max-w-3xl min-h-4 rounded-xl overflow-hidden shadow-xl m-4 flex">
-          <img src={image} className=" h-[400px] aspect-{6 / 8} object-none " />
+      <div className="py-24 w-full flex items-center justify-center flex-col px-4 gap-4">
+        <h1 className=" text-3xl">About Me</h1>
+        <div className="bg-surface w-full max-w-3xl min-h-4 rounded-xl overflow-hidden shadow-xl flex flexible">
+          <img src={image} className="aspect-square object-none " />
           <div className="p-4">
             <div>
               This will be the About Me page! For now, I really don't have anything to say here so
