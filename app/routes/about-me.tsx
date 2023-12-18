0d2642a76d020e85b9266c319a1bf42381329d83
ChrisVL-W <chris.vanlanenwanek@gmail.com>
2023-12-17 19:57:05 -0600

    Redesign about me page

--- app/routes/about-me.tsx
+++ app/routes/about-me.tsx
@@ -3,9 +3,9 @@ const image = "https://i.imgur.com/7VP7DiQ.jpg"
 export default function AboutMe() {
   return (
     <>
-      <div className="py-16 w-full h-screen flex items-center flex-col px-4 gap-4 bg-surface">
-        <h1 className=" text-5xl">About Me</h1>
-        <div className="w-full max-w-3xl min-h-4 rounded-xl overflow-hidden flex flexible gap-6">
+      <div className="w-full h-screen flex items-center flex-col gap-4 bg-surface-bg px-4">
+        <div className="w-full h-[200px] object-cover about-hero-image bg-cover bg-center absolute" />
+        <div className="w-full max-w-3xl min-h-4 rounded-2xl overflow-hidden flex flexible gap-6 z-[2] bg-surface p-4 mt-28 shadow-xl">
           <img
             src={image}
             className="aspect-square object-cover rounded-full w-[300px] self-start"
