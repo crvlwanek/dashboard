57b6e8cbd617d278d4c2ca9b06727b8b9ad0bf89
ChrisVL-W <chris.vanlanenwanek@gmail.com>
2023-12-27 22:48:09 -0600

    Improve progress bar

--- app/components/ProgressBar.tsx
+++ app/components/ProgressBar.tsx
@@ -1,14 +1,41 @@
+import * as React from "react"
 import { useNavigation } from "@remix-run/react"
 
 export default function ProgressBar() {
   const navigation = useNavigation()
-  if (navigation.state === "idle") {
-    return null
-  }
+  const active = navigation.state !== "idle"
+
+  const ref = React.useRef<HTMLDivElement>(null)
+  const [animationComplete, setAnimationComplete] = React.useState(true)
+
+  React.useEffect(() => {
+    if (!ref.current) {
+      return
+    }
+    if (active) {
+      setAnimationComplete(false)
+    }
+
+    Promise.allSettled(ref.current.getAnimations().map(({ finished }) => finished)).then(
+      () => !active && setAnimationComplete(true)
+    )
+  }, [active])
+
   return (
     <div
       role="progressbar"
-      className={`loadingIndicator fixed inset-0 h-[2px] w-2/4 bg-primary-light z-50`}
-    />
+      aria-hidden={!active}
+      aria-valuetext={active ? "Loading" : undefined}
+      className="fixed inset-x-0 top-0 left-0 z-50 h-[2px]"
+    >
+      <div
+        ref={ref}
+        className={`h-full bg-primary-main transition-all duration-500 ease-in-out ${
+          navigation.state === "idle" && animationComplete && "w-0 opacity-0 transition-none"
+        } ${navigation.state === "submitting" && "w-4/12"} ${
+          navigation.state === "loading" && "w-4/6"
+        } ${navigation.state === "idle" && !animationComplete && "w-full"}`}
+      />
+    </div>
   )
 }
